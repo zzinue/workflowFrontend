7bf71fee9d1c08427a3e50da2e9b1ad5704b38e3
zzinue <zzinue@gmail.com>
2022-05-03 00:05:39 -0500

    addint the workflow of the app on the frontend

---
+++ index.js
@@ -0,0 +1,20 @@
+const nextPAge = (e) => {
+    e.preventDefault();
+    window.location.href = "secondPage.html";
+}
+const buyingPage = (e) => {
+    e.preventDefault();
+    window.location.href = "buyingPage.html";
+}
+const login = (e) => {
+    e.preventDefault();
+    window.location.href = "login.html";
+}
+const infoProduct = (e) => {
+    e.preventDefault();
+    window.location.href = "infoProduct.html";
+}
+const purchase = (e) => {
+    e.preventDefault();
+    window.location.href = "purchase.html";
+}
